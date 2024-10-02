96a69d1ac5a71a3306c8d52d2269d8265956590b
DAVI ALEX PAIVA GALVAO <davi_galvao@estudante.sc.senai.br>
2024-10-01 21:15:00 -0300

    First Commit

---
+++ src/main/java/edu/senai/br/projetosaiot/ProjetoSaIot.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.senai.br.projetosaiot;
+
+/**
+ *
+ * @author davi_galvao
+ */
+public class ProjetoSaIot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
